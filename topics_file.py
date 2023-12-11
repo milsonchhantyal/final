5392c979ac8b3963e040afb928886d844816d07b
milsonchhantyal <153038826+milsonchhantyal@users.noreply.github.com>
2023-12-11 06:38:03 -0500

    Update topics_file.py

--- topics_file.py
+++ topics_file.py
@@ -1,4 +1,4 @@
 colors = ['blue','green','yellow','purple','red','orange','violet','pink','black','white']
 animals = ['dog', 'cat', 'horse', 'turtle', 'elephant', 'tiger', 'hippopotamus','cheetah','panda','giraffe']
 countries = ['unitedstates','mexico','canada','india','china','nepal','japan','australia','france','finland','zimbabwe','denmark',
-             'chile', 'dominicanrepublic', 'ethipoia', 'jamaica', 'honduras']
+             'chile', 'dominicanrepublic', 'ethiopia', 'jamaica', 'honduras']
