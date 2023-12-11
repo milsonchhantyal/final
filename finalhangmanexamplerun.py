125d0c4d53a42cbe8c0337a6279ca3fa13fade43
milsonchhantyal <153038826+milsonchhantyal@users.noreply.github.com>
2023-12-11 04:59:04 -0500

    Add files via upload

---
+++ finalhangmanexamplerun.py
@@ -0,0 +1,119 @@
+"""
+Welcome to hangman! Choose a topic to start playing:
+1. Colors
+2. Animals
+3. Countries
+Enter topic number: 1
+Current Word: _ _ _ _ _ _
+Attempts left: 6
+
+     ------
+     |    |
+          |
+          |
+          |
+          |
+    ---------
+    
+Guess a letter: a
+Current Word: _ _ _ _ _ _
+Attempts left: 5
+
+     ------
+     |    |
+     O    |
+          |
+          |
+          |
+    ---------
+    
+Guess a letter: e
+Current Word: _ _ _ _ e _
+Attempts left: 5
+
+     ------
+     |    |
+     O    |
+          |
+          |
+          |
+    ---------
+    
+Guess a letter: i
+Current Word: _ i _ _ e _
+Attempts left: 5
+
+     ------
+     |    |
+     O    |
+          |
+          |
+          |
+    ---------
+    
+Guess a letter: o
+Current Word: _ i o _ e _
+Attempts left: 5
+
+     ------
+     |    |
+     O    |
+          |
+          |
+          |
+    ---------
+    
+Guess a letter: u
+Current Word: _ i o _ e _
+Attempts left: 4
+
+     ------
+     |    |
+     O    |
+     |    |
+          |
+          |
+    ---------
+    
+Guess a letter: l
+Current Word: _ i o l e _
+Attempts left: 4
+
+     ------
+     |    |
+     O    |
+     |    |
+          |
+          |
+    ---------
+    
+Guess a letter: r
+Current Word: _ i o l e _
+Attempts left: 3
+
+     ------
+     |    |
+     O    |
+    /|    |
+          |
+          |
+    ---------
+    
+Guess a letter: t
+Current Word: _ i o l e t
+Attempts left: 3
+
+     ------
+     |    |
+     O    |
+    /|    |
+          |
+          |
+    ---------
+    
+Guess a letter: v
+Do you want to play again? (yes/no):
+no
+Thanks for playing!
+
+"""
---
+++ finalhangmantest.py
@@ -0,0 +1,34 @@
+"""
+Trying:
+    topics_file.colors
+Expecting:
+    ['blue', 'green', 'yellow', 'purple', 'red', 'orange', 'violet', 'pink', 'black', 'white']
+ok
+Trying:
+    topics_file.animals
+Expecting:
+    ['dog', 'cat', 'horse', 'turtle', 'elephant', 'tiger', 'hippopotamus', 'cheetah', 'panda', 'giraffe']
+ok
+Trying:
+    topics_file.countries
+Expecting:
+    ['unitedstates', 'mexico', 'canada', 'india', 'china', 'nepal', 'japan', 'australia', 'france', 'finland', 'zimbabwe', 'denmark', 'chile', 'dominicanrepublic', 'ethiopia', 'jamaica', 'honduras']
+ok
+Trying:
+    update_display('hangman', ['_', '_', '_', '_', '_', '_', '_'], 'a')
+Expecting:
+    ['_', 'a', '_', '_', '_', 'a', '_']
+ok
+5 items had no tests:
+    __main__
+    __main__.display_current_state
+    __main__.get_valid_guess
+    __main__.initialize_game
+    __main__.play_hangman
+2 items passed all tests:
+   3 tests in __main__.choose_topic_menu
+   1 tests in __main__.update_display
+4 tests in 7 items.
+4 passed and 0 failed.
+Test passed.
+"""
